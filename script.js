b20e81abf2da339866aca27c2f8e655939f60275
mPeskov23 <sorrowdelivery@gmail.com>
2024-04-22 12:51:45 +0200

    stylesheet added

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     let loggedIn = false;
-    const regForm = document.querySelector("#form-register-div");
+    const loginForm = document.querySelector("#form-login-div");
     const app = document.querySelector("#app");
 
     if(!loggedIn){
-        regForm.classList.remove("d-none");
-        regForm.classList.add("container");
+        loginForm.classList.remove("d-none");
+        loginForm.classList.add("container");
         app.classList.add("d-none");
     } else{
-        regForm.classList.add("d-none");
+        loginForm.classList.add("d-none");
         app.classList.remove("d-none");
         app.classList.add("container");
     }
