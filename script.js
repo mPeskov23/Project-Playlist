528d181c9bffd20cc8d6f89752cf69abba99fa72
mPeskov23 <sorrowdelivery@gmail.com>
2024-04-23 10:49:05 +0200

    Started rewriting script.js from scratch

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let loggedIn = false;
+let loggedIn = false;
+let userData = undefined;
+
+while(true){
     const loginForm = document.querySelector("#form-login-div");
     const app = document.querySelector("#app");
-
-    if(!loggedIn){
-        loginForm.classList.remove("d-none");
-        loginForm.classList.add("container");
-        app.classList.add("d-none");
-    } else{
-        loginForm.classList.add("d-none");
-        app.classList.remove("d-none");
+    if(loggedIn){
+        app.classList.remove("d-none")
         app.classList.add("container");
+        loginForm.classList.remove("container");
+        loginForm.classList.add("d-none");
+        application();
+    } else{
+        loginForm.classList.remove("d-none")
+        loginForm.classList.add("container");
+        app.classList.remove("container");
+        app.classList.add("d-none");
+        loginForm()
     }
-});
-
-function login(){
-    const input_login = document.querySelector("#input-login");
-    const input_password = document.querySelector("#input-password");
-    const userName = input_login.textContent;
-    const password = input_password.textContent;
-    if(checkUsername(userName)){
-        if(checkPassword(password)){
-
-        }
-    }
-}
-
-function checkUsername(username){
-    
 }
