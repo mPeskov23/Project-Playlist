4e61d5560e9332ac8b8f997ed69a7e5fe84692f5
mPeskov23 <sorrowdelivery@gmail.com>
2024-04-23 12:43:15 +0200

    Changed an infinite loop for an asyncronous function to check user's state

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
 let loggedIn = false;
-let userData = undefined;
+let usersData = undefined;
 
-while(true){
-    const loginForm = document.querySelector("#form-login-div");
-    const app = document.querySelector("#app");
-    if(loggedIn){
-        app.classList.remove("d-none")
-        app.classList.add("container");
-        loginForm.classList.remove("container");
-        loginForm.classList.add("d-none");
-        application(userData);
-    } else{
-        loginForm.classList.remove("d-none")
-        loginForm.classList.add("container");
-        app.classList.remove("container");
-        app.classList.add("d-none");
-        login();
+checkLoginState();
+
+async function checkLoginState() {
+    usersData = await getUsersData();
+    if (loggedIn) {
+        showApplication();
+    } else {
+        showLoginForm();
     }
+}
+
+function showApplication() {
+    const app = document.querySelector("#app");
+    const loginForm = document.querySelector("#form-login-div");
+    
+    app.classList.remove("d-none");
+    app.classList.add("container");
+    
+    loginForm.classList.remove("container");
+    loginForm.classList.add("d-none");
+    
+}
+
+function showLoginForm() {
+    const app = document.querySelector("#app");
+    const loginForm = document.querySelector("#form-login-div");
+    
+    app.classList.remove("container");
+    app.classList.add("d-none");
+    
+    loginForm.classList.remove("d-none");
+    loginForm.classList.add("container");
+
+    console.log(usersData);
+    
+    login();
+}
+
+function login() {
+    let login = document.querySelector("#input-login");
+    let password = document.querySelector("#input-password");
+    
+}
+
+async function getUsersData() {
+    const response = await fetch("http://localhost:3000/users");
+    const data = await response.json();
+    return data;
 }
