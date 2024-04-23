c296ef9c13172f97a4a78a865fdf1b048905e5c0
mPeskov23 <sorrowdelivery@gmail.com>
2024-04-23 15:52:22 +0200

    Buggy implementation of song submission

--- script.js
+++ script.js
@@ -29,12 +29,33 @@ initialize();
 function showApplication(userData) {
   const app = document.querySelector("#app");
   const loginForm = document.querySelector("#form-login-div");
+  const listSongs = document.querySelector("#list-songs");
+  const submitSongForm = document.querySelector("#form-submit");
+  submitSongForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const song = getSongData();
+    submitSong(event, userData, song);
+  });
 
   app.classList.remove("d-none");
   app.classList.add("container");
 
   loginForm.classList.remove("container");
   loginForm.classList.add("d-none");
+
+  let songs = userData.songs;
+  if(songs.length === 0){
+    let noSongNotification = document.createElement('p');
+    noSongNotification.textContent = "There are no songs here yet, try adding some!";
+    listSongs.appendChild(noSongNotification);
+  } else{
+    for(let song of songs){
+        let songCard = document.createElement('div');
+        songCard.innerHTML = getSongCard(song);
+        songCard.classList.add("bg-orange");
+        listSongs.appendChild(songCard);
+    }
+  }
 
 }
 
@@ -49,7 +70,10 @@ function showLoginForm() {
   loginForm.classList.add("container");
 
   loginForm = document.querySelector("#form-login");
-  loginForm.addEventListener("submit", (event) => login(event));
+  loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    login(event);
+  });
 }
 
 function login(event) {
@@ -67,12 +91,12 @@ function login(event) {
         showApplication(userData);
         break;
       } else {
-        inputPassword.value = ""; // Use value instead of textContent
+        inputPassword.value = "";
         alert("Incorrect password");
       }
     } else {
-      inputLogin.value = ""; // Use value instead of textContent
-      inputPassword.value = ""; // Use value instead of textContent
+      inputLogin.value = "";
+      inputPassword.value = "";
       alert("Incorrect username");
     }
   }
@@ -82,4 +106,36 @@ async function getFullData() {
   const response = await fetch("http://localhost:3000/users");
   const data = await response.json();
   return data;
+}
+
+function getSongCard(song){
+    return "<span>Title: " + song.title + " Artist: " + song.artist + " Genre: " + song.genre + "</span>";
+}
+
+function getSongData(){
+    const titleElement = document.querySelector("#input-title");
+    const artistElement = document.querySelector("#input-artist");
+    const genreElement = document.querySelector("#input-genre");
+    const title = titleElement.value;
+    const artist = artistElement.value;
+    const genre = genreElement.value;
+    let song = new Song(title, artist, genre);
+    titleElement.value = "";
+    artistElement.value = "";
+    genreElement.value = "";
+    return song;
+}
+
+async function submitSong(event, userData, song){
+    event.preventDefault();
+    userData.songs.push(song);
+    const updateResponse = await fetch(`http://localhost:3000/users/${userData.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(userData),
+    });
+    const listSongs = document.querySelector("#list-songs");
+    const songCard = document.createElement('div');
+    songCard.innerHTML = getSongCard(song);
+    songCard.classList.add("bg-orange");
+    listSongs.appendChild(songCard);
 }
